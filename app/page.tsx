5de04d01da8603b09b296f4a788ff7d9bccb9251
Bikaze <clmntmugisha@gmail.com>
2025-07-15 01:37:20 +0200

    Add CI workflows and update .github configuration

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { ModeToggle } from "@/components/common/mode-toggle";
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className=" w-full h-full mt-20 ">
+      <div className="mx-auto w-fit space-y-10">
+        <div className="w-fit mx-auto">
+          <ModeToggle />
+        </div>
+        <p className="font-bold text-2xl text-center">TalentRadar!</p>
+        <p className="mx-auto w-fit">Welcome</p>
+        <Button className="flex mx-auto">Get started</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
