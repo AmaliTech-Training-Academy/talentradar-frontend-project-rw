628e120e087fb300b7600ea9337ce713b45a0c61
Favor-star <favoureliab@gmail.com>
2025-07-15 12:59:23 +0200

    feat: created dashboard sidebar and navbar layout

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,39 @@
+import { AccountInfo } from "@/components/feature-specific/dashboard/account-info";
+import { AppSidebar } from "@/components/feature-specific/dashboard/app-sidebar";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { ReactNode } from "react";
+
+export default function Page({ children }: { children: ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 py-2">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          <div className="flex gap-2 ">
+            <p className="p-2 bg-violet h-10 w-10 flex items-center justify-center text-xl aspect-square font-black rounded-sm text-white">
+              TR
+            </p>
+            <div className="flex flex-col font-bold text-xl">
+              TalentRadar.AI
+              <p className="text-xs font-normal">Mini Version</p>
+            </div>
+          </div>
+          <div className="flex-1  flex justify-end h-full">
+            <AccountInfo />
+          </div>
+        </header>
+        <div className="w-full h-full  p-3">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
---
+++ components/feature-specific/dashboard/app-sidebar.tsx
@@ -0,0 +1,112 @@
+import * as React from "react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+import {
+  Bell,
+  ChartBarBig,
+  ChartColumn,
+  FileText,
+  GitBranch,
+  Home,
+  Map,
+  Settings,
+} from "lucide-react";
+import { SidebarAccountInfo } from "./sidebar-account-info";
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader className="h-18">
+        <div className="flex gap-2 h-18">
+          <p className="p-2 bg-violet h-10 w-10 flex items-center justify-center text-xl aspect-square font-black rounded-sm text-white">
+            TR
+          </p>
+          <div className="flex flex-col font-bold text-xl">
+            TalentRadar.AI
+            <p className="text-xs font-normal">Mini Version</p>
+          </div>
+        </div>
+        <Separator orientation="horizontal" />
+      </SidebarHeader>
+      <SidebarContent>
+        {/* We create a SidebarGroup for each parent. */}
+        <SidebarGroup>
+          {/* <SidebarGroupLabel>{item.title}</SidebarGroupLabel> */}
+          <SidebarGroupContent>
+            <SidebarMenu className="space-y-3">
+              {items.map(({ title, isActive, url, icon: Icon }) => (
+                <SidebarMenuItem key={title}>
+                  <SidebarMenuButton asChild isActive={isActive}>
+                    <Link href={url}>
+                      <Icon size={20} strokeWidth={1.5} />
+                      {title}
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+      <SidebarFooter>
+        <SidebarAccountInfo />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
+const items = [
+  {
+    title: "Dashabord",
+    url: "#",
+    isActive: true,
+    icon: Home,
+  },
+  {
+    title: "Productivity scored",
+    url: "#",
+    icon: ChartColumn,
+  },
+  {
+    title: "AI Roadmap & Tests",
+    url: "#",
+    icon: Map,
+  },
+  {
+    title: "Self-assessment",
+    url: "#",
+    icon: FileText,
+  },
+  {
+    title: "Notifications",
+    url: "#",
+    icon: Bell,
+  },
+  {
+    title: "Developer Flow",
+    url: "#",
+    icon: GitBranch,
+  },
+  {
+    title: "Micro services",
+    url: "#",
+    icon: ChartBarBig,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
