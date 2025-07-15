b9c38fe7cad5db490f04f14f3e2c523affbcda63
Bob Karemera Shema <bobshema14@gmail.com>
2025-07-15 18:30:04 +0200

    refactor: changed Icon display in recent activity

--- components/feature-specific/dashboard/recent-activity.tsx
+++ components/feature-specific/dashboard/recent-activity.tsx
@@ -4,18 +4,21 @@ import { Activity, Brain, FileText, MessageSquare } from "lucide-react";
 const activities = [
     {
         id: 1,
+        icon: MessageSquare,
         type: 'feedback',
         title: 'Lisa Thompson provided feedback for Michael Rodriguez',
         timestamp: '13/02/2024, 12:15:00'
     },
     {
         id: 2,
+        icon: FileText,
         type: 'self-assessment',
         title: 'Michael Rodriguez submitted a self-assessment',
         timestamp: '13/02/2024, 12:15:00'
     },
     {
         id: 3,
+        icon: Brain,
         type: 'ai-score',
         title: 'AI score updated for Jane Doe',
         timestamp: '13/02/2024, 12:15:00'
@@ -31,33 +34,31 @@ export const RecentActivity = () => {
             </div>
             <div className="p-6 space-y-4">
                 {
-                    activities.map(activity => (
+                    activities.map(activity => {
+                        const { id, type, title, timestamp, icon: Icon} = activity
+                        return (
                         <div
-                            key={activity.id}
+                            key={id}
                             className='flex gap-3'
                         >
                             <div
                                 className={clsx(
                                     'p-2 rounded-md space-y-4 h-fit',
                                     {
-                                        'text-teal bg-teal/10': activity.type === 'feedback',
-                                        'text-primary bg-primary/10': activity.type === 'self-assessment',
-                                        'text-violet bg-violet/10': activity.type === 'ai-score'
+                                        'text-teal bg-teal/10': type === 'feedback',
+                                        'text-primary bg-primary/10': type === 'self-assessment',
+                                        'text-violet bg-violet/10': type === 'ai-score'
                                     }
                                 )}
                             >
-                                {
-                                    activity.title === 'Strength Identified' ? <MessageSquare className="w-4 h-4" /> :
-                                        activity.title === 'Growth Opportunity' ? <FileText className="w-4 h-4" /> :
-                                            <Brain className="w-4 h-4" />
-                                }
+                                <Icon className="w-4 h-4" />
                             </div>
                             <div className="flex flex-col">
-                                <p className="text-sm text-foreground/80">{activity.title}</p>
-                                <p className="text-xs text-muted-foreground">{activity.timestamp}</p>
+                                <p className="text-sm text-foreground/80">{title}</p>
+                                <p className="text-xs text-muted-foreground">{timestamp}</p>
                             </div>
                         </div>
-                    ))
+                    )})
                 }
             </div>
         </article>
