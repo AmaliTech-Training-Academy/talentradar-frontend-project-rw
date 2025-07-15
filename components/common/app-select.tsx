d74d2b5f1ffd1e8410fe635bd473eac6cf2854bf
princoo <princeineza@gmail.com>
2025-07-15 19:36:54 +0200

    feat(app-select): refactor AppSelect component for improved className handling and code consistency

--- components/common/app-select.tsx
+++ components/common/app-select.tsx
@@ -1,26 +1,28 @@
-'use client'
+"use client";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
 
 export const AppSelect = ({
   options,
   placeholder,
   value,
   onChangeAction,
+  className,
 }: {
-  value: string
-  onChangeAction: (value: string) => void
-  placeholder: string
-  options: { value: string; label: string }[]
+  value: string;
+  onChangeAction: (value: string) => void;
+  placeholder: string;
+  options: { value: string; label: string }[];
+  className?: string;
 }) => {
   return (
     <Select value={value} onValueChange={onChangeAction}>
-      <SelectTrigger className="w-full">
+      <SelectTrigger className={className}>
         <SelectValue placeholder={placeholder} />
       </SelectTrigger>
       <SelectContent>
@@ -31,5 +33,5 @@ export const AppSelect = ({
         ))}
       </SelectContent>
     </Select>
-  )
-}
+  );
+};
