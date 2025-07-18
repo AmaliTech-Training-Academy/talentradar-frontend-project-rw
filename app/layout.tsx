c8d5d35c2fb724ca99304b40d764b5c58d113151
princoo <princeineza@gmail.com>
2025-07-18 11:25:21 +0200

    refactor(storeprovider): refactored the store provider location directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
-import { StoreProvider } from "../components/providers/store-provider";
-// import StoreProvider from "./StoreProvider";
+import StoreProvider from "@/components/providers/store-provider";
 
 const inter = Inter({
   subsets: ["latin"],
