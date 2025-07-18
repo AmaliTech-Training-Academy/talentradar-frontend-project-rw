fcd5f715cfeff8fa99d1e897213ce033b79c743b
princoo <princeineza@gmail.com>
2025-07-18 11:45:23 +0200

    fix(store): added the notification reducer to the store

--- lib/store.ts
+++ lib/store.ts
@@ -1,15 +1,15 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import paginationReducer from "./features/paginationslice";
-import {
-  persistReducer,
-} from "redux-persist";
+import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import persistStore from "redux-persist/es/persistStore";
 import authReducer from "./features/authSlice";
+import notificationReducer from "./features/notificationSlice";
 
 const rootReducer = combineReducers({
   pagination: paginationReducer,
   auth: authReducer,
+  notifications: notificationReducer,
 });
 const persistConfig = {
   key: "root",
