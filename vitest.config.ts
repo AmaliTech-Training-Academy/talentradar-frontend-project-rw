f7083702671a68ccff2df084f50eb9f4d305a4ad
Bob Karemera Shema <bobshema14@gmail.com>
2025-07-26 14:02:48 +0200

    test: added notifications slice and notification api tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitest/config'
+import path from 'path';
 
 export default defineConfig({
   test: {
@@ -7,4 +8,9 @@ export default defineConfig({
     setupFiles: 'vitest.setup.ts',
     include: ['**/*.{test,spec}.{ts,tsx}'],
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"), // Change to "./" if your root is not /src
+    },
+  }
 });
